f4155ed565ca81bdc2cece4a25c34be0c2ee8fe8
bz-lin <56818112+bz-lin@users.noreply.github.com>
2020-04-22 21:35:07 +0800

    Update bulid_monitor.sh

--- bulid_monitor.sh
+++ bulid_monitor.sh
@@ -1,27 +1,26 @@
 ### build monitor with raspberry buster lite img ###
 
-#enable ssh service
+##enable ssh service##
 #systemctl enable ssh
 
-#change password for pi
+##change password for pi##
 #echo pi:monitor | chpasswd
 
-#Rotate touchscreen
+##Rotate touchscreen##
 #echo "lcd_rotate=2" >> /boot/config.txt
 
-#change mirror for raspi
+##?change mirror for raspi?##
 #http://free.nchc.org.tw/raspbian/raspbian
 #echo "deb http://free.nchc.org.tw/raspbian/raspbian/ buster" > /etc/apt/sources.list     
 
-#update for raspi
+##update for raspi##
 apt update
 
-#install vsftpd.service & setting
+##install vsftpd.service & setting##
 #apt install vsftpd -y
-
 #echo "write_enable=Yes" >> /etc/vsftpd.conf
 
-#install kivy ver.1.11.1
+##?install kivy ver.1.11.1?##
 #apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
 #pkg-config libgl1-mesa-dev libgles2-mesa-dev \
 #python-setuptools libgstreamer1.0-dev git-core \
@@ -33,9 +32,12 @@ apt update
 #pip3 install setuptools
 #pip3 install Cython
 #wget https://kivy.org/downloads/1.11.1/Kivy-1.11.1.tar.gz
+#wget https://kivy.org/downloads/1.11.1/Kivy_examples-1.11.1-py2.py3-none-any.whl
+#pip3 install Kivy_examples-1.11.1-py2.py3-none-any.whl
 #tar -xvaf Kivy-1.11.1.tar.gz
 #cd  Kivy-1.11.1
 #pip3 install .
 
-#install raspap  
+
+##install raspap##
 curl -sL https://install.raspap.com | bash -s -- -y
